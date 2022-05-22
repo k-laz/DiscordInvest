83dc8e2d93d879fc1c7c922d877d5ac06d24a639
Zohaib <zrana1214@gmail.com>
2022-05-22 09:37:16 -0400

    added transaction method

--- DataBase.py
+++ DataBase.py
@@ -1,5 +1,6 @@
 from curses import noecho
 import os, psycopg2, time, logging
+from sqlite3 import connect
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,11 +16,36 @@ class dataBase:
                 "CREATE TABLE IF NOT EXISTS accounts (id STRING PRIMARY KEY, balance INT)"
             )
             cur.execute(
-                "UPSERT INTO accounts (id, balance) VALUES ('zdon', 1500)")
+                "UPSERT INTO accounts (id, balance) VALUES (%s, %s)", (username, balance))
             logging.debug("create_accounts(): status message: %s", cur.statusmessage)
         self.conn.commit()
 
 
+    # this method is called when a user makes a transaction (buy/sell)
+    # amount should be negative for sell
+    def transaction(self, username, amount):
+        with self.conn.cursor() as cur:
+
+            # Check the current balance.
+            cur.execute("SELECT balance FROM accounts WHERE id = %s", (username,))
+            from_balance = cur.fetchone()[0]
+            if from_balance < amount:
+                raise RuntimeError(
+                    f"insufficient funds in {username}: have {from_balance}, need {amount}"
+                )
+
+            # transaction.
+            cur.execute(
+                "UPDATE accounts SET balance = balance + %s WHERE id = %s", (amount, username)
+            )
+
+        self.conn.commit()
+        logging.debug("transfer_funds(): status message: %s", cur.statusmessage)
+
+
+    
+
+    # tester methods
     def print_balances(self) -> None:
         with self.conn.cursor() as cur:
             cur.execute("SELECT id, balance FROM accounts")
@@ -40,13 +66,12 @@ class dataBase:
 
 
 def main():
-    connect = dataBase()
+    portfolio = dataBase()
+    portfolio.create_account("zdon", 100000)
+    portfolio.print_balances()
 
-    connect.create_account("zdon", 100000)
-
-    connect.print_balances()
-    connect.delete_accounts()
-    connect.print_balances()
+    portfolio.transaction("zdon", -55000)
+    portfolio.print_balances()
 
 if __name__ == "__main__":
     main()
