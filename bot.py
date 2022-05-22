3e46b4f2aa159cc624abd76309297e7f90194991
k-laz <klazarev2000@gmail.com>
2022-05-21 19:45:56 -0700

    basic logic with user class added

--- bot.py
+++ bot.py
@@ -3,73 +3,55 @@ import os
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
-
 import logging
-
-# logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-intents = discord.Intents.default()
-intents.message_content = True
+#logging.basicConfig(level=logging.INFO)
+
+class User:
+    def __init__(self, id):
+        self.id = id
+        self.cash = 100000
+        self.stocks = {}
+
+    def purchaseStock(self, stockName, amount):
+        if stockName in self.stocks:
+            self.stocks[stockName] += amount
+        else:
+            self.stocks[stockName] = amount
+        
 
 
-# client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='$')
 
-# @client.event
-# async def on_ready():
-#     print(f'We have logged in as {client.user}')
-
-# @client.event
-# async def on_member_join(member):
-#     await member.create_dm()
-#     await member.dm_channel.send(
-#         f'Hi {member.name}, welcome to my Discord server!'
-#     )
-
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-
-#     if message.content.startswith('$hello'):
-#         await message.channel.send('Hello!')
-bot = commands.Bot(command_prefix='.')
+users = {}
 
 @bot.event
 async def on_ready():
-    print("Bot is ready for use")
+    print('Loading database of users')
+    # Load the dictionary from cockroachDB
+    print(f'{bot.user.name} has connected to Discord!')
 
-@bot.command()
-async def ping(context):
-    await context.send(f'Pong! {round(bot.latency * 1000)}ms')
+@bot.command(name='invest')
+async def invest(ctx):
+    #await ctx.send(ctx.author)
+    userid = ctx.author.id
+    
+    if userid not in users:
+        user = User(ctx.author.id)
+        users[userid] = user
+    else:
+        user = users[userid]
 
-@bot.command()
-async def buy(ctx):
-    await ctx.send(f'Bought this many shares')
+    user.purchaseStock('TSLA', 123)
+
+    x = user.stocks.get('TSLA')
+    await ctx.send(f'you just purchased {x} of TSLA')
+
+
 
 bot.run(TOKEN)
 
-# from discord.ext import commands
-# bot = commands.Bot(command_prefix='.')
-
-# @bot.event
-# async def on_ready():
-#     print("Bot is ready for use")
-
-# @bot.command()
-# async def ping(context):
-#     await context.send(f'Pong! {round(bot.latency * 1000)}ms')
-
-# @bot.command()
-# async def buy(ctx):
-#     await ctx.send(f'Bought this many shares')
-
-# bot.run(TOKEN)
 
